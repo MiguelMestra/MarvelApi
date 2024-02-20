f5c088f295edadaa9a43c7cc73b190a0f7c4c7e4
MiguelMestra <miguelangelmestra14@outlook.com>
2024-02-19 21:23:44 -0300

    filter params to call characters

--- src/main/java/com/example/marvel/controller/CharacterController.java
+++ src/main/java/com/example/marvel/controller/CharacterController.java
@@ -3,10 +3,7 @@ package com.example.marvel.controller;
 import com.example.marvel.dto.PaginatedResponse;
 import com.example.marvel.service.MarvelApiService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URISyntaxException;
 
@@ -20,9 +17,9 @@ public class CharacterController {
 
     @GetMapping("/character")
     @ResponseStatus(HttpStatus.OK)
-    public PaginatedResponse getCharacter(@PathVariable (required = false) String name,
-                                          @PathVariable (required = false) String series,
-                                          @PathVariable (required = false) String stories) throws URISyntaxException {
+    public PaginatedResponse getCharacter(@RequestParam(required = false) String name,
+                                          @RequestParam (required = false) String series,
+                                          @RequestParam (required = false) String stories) throws URISyntaxException {
         return marvelApiService.getAllCharacters(name,series,stories);
     }
 }
--- src/main/java/com/example/marvel/service/MarvelApiService.java
+++ src/main/java/com/example/marvel/service/MarvelApiService.java
@@ -2,13 +2,16 @@ package com.example.marvel.service;
 
 import com.example.marvel.dto.PaginatedResponse;
 import com.example.marvel.utils.HashUtil;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 @Service
@@ -28,19 +31,29 @@ public class MarvelApiService {
 
 
     public PaginatedResponse getAllCharacters(String name, String series, String stories) throws URISyntaxException {
+        ArrayList<NameValuePair> filterParameters = new ArrayList<>();
+        if (Objects.nonNull(name)){
+            filterParameters.add(new BasicNameValuePair("name",name));
+        }
+        if (Objects.nonNull(series)){
+            filterParameters.add(new BasicNameValuePair("series",series));
+        }
+        if (Objects.nonNull(stories)){
+            filterParameters.add(new BasicNameValuePair("stories",stories));
+        }
 
-        URI uri = getUri(ALL_CHARACTER_PATH);
-
+        URI uri = getUri(ALL_CHARACTER_PATH,filterParameters);
         return restTemplate.getForEntity(uri, PaginatedResponse.class).getBody();
     }
 
-    private URI getUri(String path) throws URISyntaxException {
+    private URI getUri(String path, ArrayList<NameValuePair> aditionalParameters) throws URISyntaxException {
         String timestamp = String.valueOf(System.currentTimeMillis() );
         String hash = HashUtil.getMD5(timestamp+privateKey+publicKey);
         return new URIBuilder(BASE_URL+path)
                 .addParameter("ts", timestamp)
                 .addParameter("apikey",publicKey)
                 .addParameter("hash", hash)
+                .addParameters(aditionalParameters)
                 .build();
     }
 }
